2574d32be3b0a0b3ac27e9456eb9345d2b9c6435
Tomasz Mach <m4chtomasz@gmail.com>
2024-06-12 18:01:26 +0200

    Add: Milestone 3, 4, 5 ;D-

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="row">
+    <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+
+
+<script>
+
+import axios from 'axios'
+import ProjectCard from './components/ProjectCard.vue'
+
+export default {
+  components: {
+    ProjectCard
+  },
+
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  methods: {
+
+    fetchProject() {
+
+      axios.get('http://127.0.0.1:8000/api/projects', {
+
+      })
+        .then((res) => {
+          // console.log(res.data.posts) // senza la paginazione
+          console.log(res.data)
+          this.projects = res.data.results
+        })
+
+    }
+  },
+  created() {
+    this.fetchProject()
+  },
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.row {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,55 @@
+<template>
+
+    <div class="">
+        <div class="project-card">
+            <h2>Title:</h2>
+            <h3>{{ project.title }}</h3>
+            <!-- <p>{{ project.description }}</p> -->
+            <div v-if="project.technologies && project.technologies.length">
+                <h3>Technologies:</h3>
+                <span v-for="tech in project.technologies" :key="tech.id">{{ tech.name }}-</span>
+            </div>
+            <div v-else>
+                <h3>Technologies:</h3>
+                <p>not available</p>
+            </div>
+            <div v-if="project.type">
+                <h3>Project type:</h3>
+                <p>{{ project.type.name }}</p>
+
+            </div>
+            <div v-else>
+                <h3>Project type:</h3>
+                <p>not available</p>
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style scoped>
+.project-card {
+    border: 1px solid #ccc;
+    padding: 16px;
+    margin: 16px 0;
+    width: 300px;
+    height: 600px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+}
+</style>
