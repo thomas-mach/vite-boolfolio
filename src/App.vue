e73e7723460a3a33ba25435b8d5969dd60d6c799
Tomasz Mach <m4chtomasz@gmail.com>
2024-06-13 09:24:24 +0200

    Add: paginate

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,19 @@
   <div class="container mt-6 mb-6">
     <div class="grid grid-cols-3 gap-4">
       <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
-
     </div>
   </div>
+
+  <div v-if="lastPage > 1">
+    <ul class="flex gap-3 justify-center my-8">
+      <li :class="n === currentPage ? 'bg-amber-400' : 'bg-gray-100'"
+        class="cursor-pointer aspect-square w-8 flex justify-center items-center rounded-full " @click="changePage(n)"
+        v-for="n in lastPage" :key="n">
+        {{ n }}
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 
@@ -22,20 +32,32 @@ export default {
   data() {
     return {
       projects: [],
+      currentPage: 1,
+      lastPage: null
     }
   },
 
   methods: {
+    changePage(n) {
+      if (n === this.currentPage) return
+      this.currentPage = n
+      this.fetchProject()
+    },
 
     fetchProject() {
 
-      axios.get('http://127.0.0.1:8000/api/projects', {
+      axios.get('http://127.0.0.1:8000/api/projects?page=1', {
+        params: {
+          page: this.currentPage,
+        }
 
       })
         .then((res) => {
           // console.log(res.data.posts) // senza la paginazione
           console.log(res.data)
-          this.projects = res.data.results
+          this.projects = res.data.results.data
+          this.projects = res.data.results.data
+          this.lastPage = res.data.results.last_page
         })
 
     }
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -31,6 +31,10 @@
     </div>
 
 
+
+
+
+
 </template>
 
 <script>
